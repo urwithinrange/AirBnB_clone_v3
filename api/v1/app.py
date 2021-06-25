26125f51f5e85db71aee8b27448013c8b3888f86
Derek_Clemens <urwithinrange@yahoo.com>
2021-06-25 05:02:45 +0000

    changed os

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,7 +3,7 @@
 from flask import Flask
 from models import storage
 from api.v1.views import app_views
-from os import getenv
+import os
 app = Flask(__name__)
 app.register_blueprint(auth_views)
 
@@ -13,6 +13,6 @@ def tear_down(self):
     storage.close()
 
 if __name__ == "__main__":
-host = getenv("HBNB_API_HOST", "0.0.0.0")
-port = int(getenv("HBNB_API_PORT", 5000))
+host = os.getenv("HBNB_API_HOST", "0.0.0.0")
+port = int(os.getenv("HBNB_API_PORT", 5000))
 app.run(host=host, port=port, threaded=True)
