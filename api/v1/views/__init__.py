2a5fc13c4ac23d898b0b6f58383894800bbd721c
Derek_Clemens <urwithinrange@yahoo.com>
2021-06-25 05:27:43 +0000

    Changing uri to url

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -2,4 +2,4 @@
 """ Initing the stuff """
 from flask import Blueprint
 
-app_views = Blueprint("app_views", __name__, uri_prefix="/api/v1") 
+app_views = Blueprint("app_views", __name__, url_prefix="/api/v1") 
